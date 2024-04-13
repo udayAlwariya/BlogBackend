d5e3e004f2c29e8a9074df399dd880cb918f5544
udayAlwariya <ravialwariya76@gmail.com>
2024-04-13 06:18:21 +0530

    updated

--- config/zod.js
+++ config/zod.js
@@ -1,14 +1,14 @@
 const z = require("zod")
 const signupSchema = z.object({
-    firstName : z.string(),
-    lastName : z.string(),
-    email : z.string().email(),
-    password : z.string().min(8)
+    firstName: z.string().min(1, { message: "FirstName is required" }).trim(),
+    lastName: z.string().min(1, { message: "LastName is required" }).trim(),
+    email : z.string().email({message:"Enter Valid Email"}).trim(),
+    password : z.string().min(8,{message:"Password must be of 8 letters"}).trim()
 })
 
 const signinSchema = z.object({
-    email : z.string().email(),
-    password : z.string().min(8),
+    email : z.string().email({message:"Enter Valid Email"}).trim(),
+    password : z.string().min(8,{message:"Password must be of 8 letters"}).trim()
 })
 
 module.exports = {signupSchema,signinSchema}
